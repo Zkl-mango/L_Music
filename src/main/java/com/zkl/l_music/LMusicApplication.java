2c64891e98bf71275b10ec0d121cec05df0f9aba
zhengkunli <1473055523@qq.com>
2019-12-26 22:02:37 +0800

    Initial commit

---
+++ src/main/java/com/zkl/l_music/LMusicApplication.java
@@ -0,0 +1,13 @@
+package com.zkl.l_music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zkl/l_music/LMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zkl.l_music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
